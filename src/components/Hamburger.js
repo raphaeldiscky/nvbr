a3126c268faafc9f7be1fbcccead0b40aa176e2e
Raphael Discky <zundria.putra@gmail.com>
2021-03-09 21:41:39 +0700

    Animate menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,26 +1,95 @@
 import React, { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import gsap from 'gsap'
 
 const Hamburger = ({ state }) => {
+  // vars for our animated dom nodes
   let menu = useRef(null)
+  let revealMenu = useRef(null)
+  let revealMenuBackground = useRef(null)
+  let cityBackground = useRef(null)
+  let line1 = useRef(null)
+  let line2 = useRef(null)
+  let line3 = useRef(null)
+  let info = useRef(null)
 
   useEffect(() => {
     if (state.clicked === false) {
       // close our menu
-      menu.style.display = 'none'
+      gsap.to([revealMenu, revealMenuBackground], {
+        duration: 0.8,
+        height: 0,
+        ease: 'power3.inOut',
+        stagger: {
+          amount: 0.07
+        }
+      })
+      gsap.to(menu, {
+        duration: 1,
+        css: { display: 'none' }
+      })
     } else if (
       state.clicked === true ||
       (state.clicked === true && state.initial === null)
     ) {
       // open our menu
-      menu.style.display = 'block'
+      gsap.to(menu, {
+        duration: 0,
+        css: { display: 'block' }
+      })
+      gsap.to([revealMenuBackground, revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: '100%'
+      })
+      staggerReveal(revealMenuBackground, revealMenu)
+      fadeInUp(info)
+      staggerText(line1, line2, line3)
     }
-  })
+  }, [state])
+
+  const staggerReveal = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: 0.8,
+      height: 0,
+      transformOrigin: 'right top',
+      skewY: 2,
+      ease: 'power3.inOut',
+      stagger: {
+        amount: 0.1
+      }
+    })
+  }
+
+  const fadeInUp = (node) => {
+    gsap.from(node, {
+      y: 60,
+      duration: 1,
+      delay: 0.2,
+      opacity: 0,
+      ease: 'power3.inOut'
+    })
+  }
+
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      duration: 0.8,
+      y: 100,
+      delay: 0.1,
+      ease: 'power3.inOut',
+      stagger: {
+        amount: 0.3
+      }
+    })
+  }
 
   return (
     <div ref={(el) => (menu = el)} className='hamburger-menu'>
-      <div className='menu-secondary-bg-color'></div>
-      <div className='menu-layer'>
+      <div
+        ref={(el) => (revealMenuBackground = el)}
+        className='menu-secondary-bg-color'
+      ></div>
+      <div ref={(el) => (revealMenu = el)} className='menu-layer'>
         <div className='menu-city-bg'></div>
         <div className='container'>
           <div className='wrapper'>
@@ -28,17 +97,23 @@ const Hamburger = ({ state }) => {
               <nav>
                 <ul>
                   <li>
-                    <Link to='/opportunities'>Opportunities</Link>
+                    <Link ref={(el) => (line1 = el)} to='/opportunities'>
+                      Opportunities
+                    </Link>
                   </li>
                   <li>
-                    <Link to='/solutions'>Solutions</Link>
+                    <Link ref={(el) => (line2 = el)} to='/solutions'>
+                      Solutions
+                    </Link>
                   </li>
                   <li>
-                    <Link to='/contact-us'>Contact us</Link>
+                    <Link ref={(el) => (line3 = el)} to='/contact-us'>
+                      Contact us
+                    </Link>
                   </li>
                 </ul>
               </nav>
-              <div className='info'>
+              <div ref={(el) => (info = el)} className='info'>
                 <h3>Our Promise</h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
