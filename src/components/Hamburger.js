8e3957ce7f381a700a6729b29c4ef7134b85b8ff
Raphael Discky <zundria.putra@gmail.com>
2021-03-09 21:09:41 +0700

    Add header styling

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,5 +1,50 @@
+import { Link } from 'react-router-dom'
+
 const Hamburger = () => {
-  return <div></div>
+  return (
+    <div className='hamburger-menu'>
+      <div className='menu-secondary-bg-color'></div>
+      <div className='menu-layer'>
+        <div className='menu-city-bg'></div>
+        <div className='container'>
+          <div className='wrapper'>
+            <div className='menu-links'>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to='/opportunities'>Opportunities</Link>
+                  </li>
+                  <li>
+                    <Link to='/solutions'>Solutions</Link>
+                  </li>
+                  <li>
+                    <Link to='/contact-us'>Contact us</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className='info'>
+                <h3>Our Promise</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Similique fuga ipsum corrupti. Atque esse rerum adipisci ad
+                  deleniti ratione laborum velit maiores eum repellat
+                  necessitatibus, enim quo neque quia recusandae.
+                </p>
+              </div>
+              <div className='locations'>
+                Locations:
+                <span>Dallas</span>
+                <span>Austin</span>
+                <span>New York</span>
+                <span>San Francisco</span>
+                <span>Beijing</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hamburger
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
+import Hamburger from './Hamburger'
+
 const Header = () => {
-  return <div></div>
+  return (
+    <header>
+      <div className='container'>
+        <div className='wrapper'>
+          <div className='inner-header'>
+            <div className='logo'>
+              <Link to='/'>NVBR.</Link>
+            </div>
+            <div className='menu'>
+              <button>Menu</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger />
+    </header>
+  )
 }
 
 export default Header
